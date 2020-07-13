d6fdb7edf580dfcef5c2dee8cfcbf8e392c9657e
Zimomo <lzh120908717@gmail.com>
2020-07-13 23:35:40 +0800

    mock 引入 统计模块正常

--- mock/index.js
+++ mock/index.js
@@ -4,11 +4,13 @@ const { param2Obj } = require('./utils')
 const user = require('./user')
 const account = require('./account')
 const record = require('./record')
+const analysis = require('./analysis')
 
 const mocks = [
   ...user,
   ...account,
-  ...record
+  ...record,
+  ...analysis
 ]
 
 // for front mock
